bde6a0122d6a1a97145dad54eba705025bd945a4
dracero <dracero@users.noreply.github.com>
2019-05-13 12:01:07 -0300

    Initial commit

---
+++ api/DB.js
@@ -0,0 +1,4 @@
+module.exports = {
+  DB:
+    "mongodb://root:juanperez@cluster0-shard-00-00-zf9fl.mongodb.net:27017cluster0-shard-00-01-zf9fl.mongodb.net:27017,cluster0-shard-00-02-zf9fl.mongodb.net:27017/tensor?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+};
